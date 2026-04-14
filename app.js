60b21b7a2fc7433d4814b63c9d14ff8e3def4775
nakulbhadrecha202005-max <nakulbhadrecha2026@gmail.com>
2026-04-14 21:05:53 +0530

    first commit

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get("/read", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
